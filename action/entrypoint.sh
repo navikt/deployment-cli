4a9fde2ef8e5947ab9cb486546f6ad1a413aba35
Kyrre Havik Eriksen <kyrremann@gmail.com>
2019-09-04 16:56:35 +0200

    Init commit for action

---
+++ action/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -l
+
+SHORT_SHA=$(echo "$GITHUB_REF" | cut -c -6)
+if [ "$INPUT_REF" = "GITHUB_SHA" ]
+then
+    export INPUT_REF=$SHORT_SHA
+fi
+if [ "$INPUT_TAG" = "GITHUB_SHA" ]
+then
+    export INPUT_TAG=$SHORT_SHA
+fi
+
+/deployment-cli deploy create \
+		--cluster="$INPUT_CLUSTER" \
+		--team="$INPUT_TEAM" \
+		--repository="$INPUT_REPOSITORY" \
+		--resource="$GITHUB_WORKSPACE"/"$INPUT_RESOURCE" \
+		--vars="$GITHUB_WORKSPACE"/"$INPUT_VARS" \
+		--token="$GITHUB_TOKEN" \
+		--var tag="$INPUT_TAG" \
+		--ref="$INPUT_REF"
