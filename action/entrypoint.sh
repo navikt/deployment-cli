0513175c1c2fd8e571c0942c16063843977f4fb0
Kyrre Havik Eriksen <kyrremann@gmail.com>
2019-09-05 09:22:32 +0200

    Testing av vars

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh -l
 
+echo $INPUT_REF
+echo $INPUT_TAG
+cat .docker_tag
 SHORT_SHA=$(echo "$GITHUB_REF" | cut -c -6)
+echo $SHORT_SHA
 if [ "$INPUT_REF" = "GITHUB_SHA" ]
 then
     export INPUT_REF=$SHORT_SHA
@@ -9,6 +13,9 @@ if [ "$INPUT_TAG" = "GITHUB_SHA" ]
 then
     export INPUT_TAG=$SHORT_SHA
 fi
+
+echo $INPUT_REF
+echo $INPUT_TAG
 
 /deployment-cli deploy create \
 		--cluster="$INPUT_CLUSTER" \
