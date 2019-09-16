e09396eed75732a0c1de7de2e783fa22119bad3a
Kyrre Havik Eriksen <kyrremann@gmail.com>
2019-09-16 12:38:09 +0200

    Check docker-files, støtte for vars

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -7,13 +7,18 @@ then
     export INPUT_REF=$SHORT_SHA
 fi
 
-if [ "$INPUT_IMAGE" = "FROM_FILE" ]
+if [ "$INPUT_IMAGE" = "FROM_FILE" ] && [ -f ".docker_image" ]
 then
     export INPUT_IMAGE=$(cat .docker_image)
 fi
-if [ "$INPUT_TAG" = "FROM_FILE" ]
+if [ "$INPUT_TAG" = "FROM_FILE" ] && [ -f ".docker_tag" ]
 then
     export INPUT_TAG=$(cat .docker_tag)
+fi
+if [ -z "$INPUT_VARS" ]
+then
+    touch .empty
+    export INPUT_VARS=".empty"
 fi
 
 deployment-cli deploy create \
@@ -24,4 +29,5 @@ deployment-cli deploy create \
 	       --token="$GITHUB_TOKEN" \
 	       --var image="$INPUT_IMAGE" \
 	       --var tag="$INPUT_TAG" \
-	       --ref="$INPUT_REF"
+	       --ref="$INPUT_REF" \
+	       --vars="$INPUT_VARS"
