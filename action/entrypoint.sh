a36487376e67cdffc4138a62bfa95f2893b5f1e5
Kent Daleng <kent.stefan.daleng@nav.no>
2019-09-27 10:55:52 +0200

    Simplify entrypoint logic

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -l
 
 SHORT_SHA=$(echo "$GITHUB_SHA" | cut -c -6)
+touch .empty
+echo "{}" > .empty.json
+
 
 if [ "$INPUT_REF" = "GITHUB_SHA" ]
 then
@@ -17,59 +20,26 @@ then
 fi
 if [ -z "$INPUT_VARS" ]
 then
-    echo "{}" > .empty
-    export INPUT_VARS=".empty"
+    export INPUT_VARS=".empty.json"
 fi
-if [ -z "$INPUT_RAWRESOURCES" ] && [ -z "$INPUT_RESOURCES" ]
-then
-    echo "Defaulting to --resource=nais.yaml"
-    deployment-cli deploy create \
-	      --cluster="$INPUT_CLUSTER" \
-	      --team="$INPUT_TEAM" \
-	      --resource="nais.yaml" \ # default to nais.yaml if neither resource fields are set
-	      --repository="$GITHUB_REPOSITORY" \
-	      --token="$GITHUB_TOKEN" \
-	      --var image="$INPUT_IMAGE" \
-	      --var tag="$INPUT_TAG" \
-	      --ref="$INPUT_REF" \
-	      --vars="$INPUT_VARS"
-elif [ -z "$INPUT_RAWRESOURCES" ] # use resources if rawresources is not set
-then
-    echo "Using provided resource $INPUT_RESOURCES"
-    deployment-cli deploy create \
-        --cluster="$INPUT_CLUSTER" \
-        --team="$INPUT_TEAM" \
-        --resource="$INPUT_RESOURCES" \
-        --repository="$GITHUB_REPOSITORY" \
-        --token="$GITHUB_TOKEN" \
-        --var image="$INPUT_IMAGE" \
-        --var tag="$INPUT_TAG" \
-        --ref="$INPUT_REF" \
-        --vars="$INPUT_VARS"
-elif [ -z "$INPUT_RESOURCES" ] # use rawresources if resources is not set
-then
-    echo "Using provided raw resources $INPUT_RAWRESOURCES"
-    deployment-cli deploy create \
-        --cluster="$INPUT_CLUSTER" \
-        --team="$INPUT_TEAM" \
-        --raw-resource="$INPUT_RAWRESOURCES" \
-        --repository="$GITHUB_REPOSITORY" \
-        --token="$GITHUB_TOKEN" \
-        --var image="$INPUT_IMAGE" \
-        --var tag="$INPUT_TAG" \
-        --ref="$INPUT_REF" \
-        --vars="$INPUT_VARS"
-else                           # use both fields if they are set
-    echo "Using both raw resources $INPUT_RAWRESOUCES and resources $INPUT_RESOURCES" 
-    deployment-cli deploy create \
-        --cluster="$INPUT_CLUSTER" \
-        --team="$INPUT_TEAM" \
-        --resource="$INPUT_RESOURCES"
-        --raw-resource="$INPUT_RAWRESOURCES" \
-        --repository="$GITHUB_REPOSITORY" \
-        --token="$GITHUB_TOKEN" \
-        --var image="$INPUT_IMAGE" \
-        --var tag="$INPUT_TAG" \
-        --ref="$INPUT_REF" \
-        --vars="$INPUT_VARS"
+if [ -z "$INPUT_RESOURCES" ]
+    export INPUT_RESOURCES=".empty"
 fi
+if [ -z "$INPUT_RAWRESOURCES" ]
+    export INPUT_RAWRESOURCES=".empty"
+fi
+if [ -z "$INPUT_RESOURCES"] && [ -z "$INPUT_RAWRESOUCES"]
+    export INPUT_RESOURCES="nais.yaml"
+fi
+
+deployment-cli deploy create \
+    --cluster="$INPUT_CLUSTER" \
+    --team="$INPUT_TEAM" \
+    --resource="$INPUT_RESOURCES"
+    --raw-resource="$INPUT_RAWRESOURCES" \
+    --repository="$GITHUB_REPOSITORY" \
+    --token="$GITHUB_TOKEN" \
+    --var image="$INPUT_IMAGE" \
+    --var tag="$INPUT_TAG" \
+    --ref="$INPUT_REF" \
+    --vars="$INPUT_VARS"
