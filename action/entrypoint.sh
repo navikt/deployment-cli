ea9e740dc0dcab70f8f50bdb65f9f528d47bada7
Kyrre Havik Eriksen <kyrremann@gmail.com>
2019-10-09 15:45:08 +0200

    Skrevet om, og støtte for liste av ressurser

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,73 +1,44 @@
 #!/bin/sh -l
+# shellcheck disable=SC2153
 
-SHORT_SHA=$(echo "$GITHUB_SHA" | cut -c -6)
-
+CMD="deployment-cli deploy create --cluster=$INPUT_CLUSTER --team=$INPUT_TEAM --repository=$GITHUB_REPOSITORY --token=$GITHUB_TOKEN"
 
 if [ "$INPUT_REF" = "GITHUB_SHA" ]
 then
-    export INPUT_REF=$SHORT_SHA
+    INPUT_REF="$(echo "$GITHUB_SHA" | cut -c -6)"
+    export INPUT_REF
 fi
+CMD="$CMD --ref=$INPUT_REF"
 
 if [ "$INPUT_IMAGE" = "FROM_FILE" ] && [ -f ".docker_image" ]
 then
-    export INPUT_IMAGE=$(cat .docker_image)
+    INPUT_IMAGE="$(cat .docker_image)"
+    export INPUT_IMAGE
 fi
+CMD="$CMD --var image=$INPUT_IMAGE"
+
 if [ "$INPUT_TAG" = "FROM_FILE" ] && [ -f ".docker_tag" ]
 then
-    export INPUT_TAG=$(cat .docker_tag)
+    INPUT_TAG="$(cat .docker_tag)"
+    export INPUT_TAG
 fi
+CMD="$CMD --var tag=$INPUT_TAG"
+
 if [ -z "$INPUT_VARS" ]
 then
     echo "{}" > .empty
     export INPUT_VARS=".empty"
 fi
+CMD="$CMD --vars=$INPUT_VARS"
 
-if [ -z "$INPUT_RESOURCES" ] && [ -z "$INPUT_RAWRESOURCES" ]
+if [ -n "$INPUT_RESOURCES" ]
 then
-    deployment-cli deploy create \
-        --cluster="$INPUT_CLUSTER" \
-        --team="$INPUT_TEAM" \
-        --resource="nais.yaml" \
-        --repository="$GITHUB_REPOSITORY" \
-        --token="$GITHUB_TOKEN" \
-        --var image="$INPUT_IMAGE" \
-        --var tag="$INPUT_TAG" \
-        --ref="$INPUT_REF" \
-        --vars="$INPUT_VARS"
-elif [ -z "$INPUT_RESOURCES" ]
-then
-    deployment-cli deploy create \
-        --cluster="$INPUT_CLUSTER" \
-        --team="$INPUT_TEAM" \
-        --raw-resource="$INPUT_RAWRESOURCES" \
-        --repository="$GITHUB_REPOSITORY" \
-        --token="$GITHUB_TOKEN" \
-        --var image="$INPUT_IMAGE" \
-        --var tag="$INPUT_TAG" \
-        --ref="$INPUT_REF" \
-        --vars="$INPUT_VARS"
-elif [ -z "$INPUT_RAWRESOURCES" ]
-then
-    deployment-cli deploy create \
-        --cluster="$INPUT_CLUSTER" \
-        --team="$INPUT_TEAM" \
-        --resource="$INPUT_RESOURCES" \
-        --repository="$GITHUB_REPOSITORY" \
-        --token="$GITHUB_TOKEN" \
-        --var image="$INPUT_IMAGE" \
-        --var tag="$INPUT_TAG" \
-        --ref="$INPUT_REF" \
-        --vars="$INPUT_VARS"
-else
-    deployment-cli deploy create \
-        --cluster="$INPUT_CLUSTER" \
-        --team="$INPUT_TEAM" \
-        --resource="$INPUT_RESOURCES" \
-        --raw-resource="$INPUT_RAWRESOURCES" \
-        --repository="$GITHUB_REPOSITORY" \
-        --token="$GITHUB_TOKEN" \
-        --var image="$INPUT_IMAGE" \
-        --var tag="$INPUT_TAG" \
-        --ref="$INPUT_REF" \
-        --vars="$INPUT_VARS"
+    CMD="$CMD --resource=$(echo "$INPUT_RESOURCES" | sed -e 's/,/ --resource /g')"
 fi
+
+if [ -n "$INPUT_RAWRESOURCES" ]
+then
+    CMD="$CMD --raw-resource=$(echo "$INPUT_RAWRESOURCES" | sed -e 's/,/ --raw-resource /g')"
+fi
+
+eval "$CMD"
